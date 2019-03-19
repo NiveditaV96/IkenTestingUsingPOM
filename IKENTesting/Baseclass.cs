c3a31ee9eb9f6cc8f1c9928a29202b7a4f6e0ac7
Divya Gottipati <EE210668@sasken.com>
2019-03-19 13:58:30 +0530

    commit

---
+++ IKENTesting/Baseclass.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKENTesting.Baseclass
+{
+    [TestClass]
+    public class BaseClass
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        #region WebElement
+        private By SDPLink = By.XPath("//span[@class='link_text' and text()='SDP']");
+        private By Username = By.XPath("//input[@id='username']");
+        private By Password = By.XPath("//input[@id='password']");
+        private By Login = By.XPath("//input[@name='login']");
+        private By TimeSheetLink = By.XPath("//a[@class='fa fa-clock-o timesheets df']");
+
+
+        #endregion
+
+        #region actions
+       
+        public void Actions()
+        {
+
+            driver.FindElement(Username).SendKeys("ee210668");
+            driver.FindElement(Password).SendKeys("System210668");
+            driver.FindElement(Login).Click();
+            driver.FindElement(TimeSheetLink).Click();
+        }
+      #endregion
+      #region Navigation
+      [TestMethod]
+        public void Navigation()
+        {
+           driver.Navigate().GoToUrl("https://iken.sasken.com/");
+           driver.Manage().Window.Maximize();
+           driver.FindElement(SDPLink).Click();
+            var currentWindow = driver.CurrentWindowHandle;
+            foreach (String winHandle in driver.WindowHandles)
+            {
+                if (winHandle != currentWindow)
+                {
+                    driver.SwitchTo().Window(winHandle);
+                }
+            }
+            Actions();
+            driver.Quit();
+        }
+
+        #endregion
+        
+       
+    }
+}
---
+++ IKENTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IKENTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IKENTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40d59110-af88-4ba5-9cd4-742e5d0df57a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
